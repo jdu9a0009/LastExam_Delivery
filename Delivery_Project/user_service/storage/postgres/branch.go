52d2ff1e90e16c2ac25217cad839036dd42c522f
Sarvarbek Fozilov <123530266+jdu9a0009@users.noreply.github.com>
2023-11-05 20:28:02 +0500

    finally finished all user service

---
+++ Delivery_Project/user_service/storage/postgres/branch.go
@@ -0,0 +1,277 @@
+package postgres
+
+import (
+	"context"
+	"database/sql"
+	"fmt"
+	"time"
+
+	user_service "user_service/genproto"
+	"user_service/pkg/helper"
+
+	"github.com/jackc/pgx"
+	"github.com/jackc/pgx/v4/pgxpool"
+)
+
+type branchRepo struct {
+	db *pgxpool.Pool
+}
+
+func NewBranch(db *pgxpool.Pool) *branchRepo {
+	return &branchRepo{
+		db: db,
+	}
+}
+
+func (b *branchRepo) Create(c context.Context, req *user_service.CreateBranchRequest) (*user_service.Response, error) {
+
+	query := `
+	  INSERT INTO branches (
+		name,  
+		phone,
+		photo,
+		delivery_tarif_id,
+		work_hour_start,
+		work_hour_end,
+		address,
+		destination,
+		created_at
+	  ) VALUES (
+		$1, $2, $3, $4, $5, $6, $7, $8, $9
+	  ) RETURNING id`
+
+	var id int
+	err := b.db.QueryRow(c, query,
+		req.Name,
+		req.Phone,
+		req.Photo,
+		req.DeliveryTarifId,
+		req.WorkHourStart,
+		req.WorkHourEnd,
+		req.Address,
+		req.Destination,
+		time.Now(),
+	).Scan(&id)
+
+	if err != nil {
+		return nil, fmt.Errorf("failed to create branch: %w", err)
+	}
+
+	return &user_service.Response{Message: fmt.Sprintf("%d", id)}, nil
+}
+func (b *branchRepo) Get(c context.Context, req *user_service.IdRequest) (resp *user_service.Branch, err error) {
+	var (
+		createdAt sql.NullString
+		updatedAt sql.NullString
+	)
+	query := `
+    SELECT 
+		id,
+		name,
+		phone,
+		photo,
+		delivery_tarif_id,
+		work_hour_start::TEXT,
+		work_hour_end::TEXT,
+		address,
+		active,
+		destination,
+		created_at::TEXT,
+		updated_at::TEXT 
+    FROM branches 
+    WHERE id = $1 AND "active" AND "deleted_at" IS NULL;`
+
+	branch := user_service.Branch{}
+
+	err = b.db.QueryRow(c, query, req.Id).Scan(
+		&branch.Id,
+		&branch.Name,
+		&branch.Phone,
+		&branch.Photo,
+		&branch.DeliveryTarifId,
+		&branch.WorkHourStart,
+		&branch.WorkHourEnd,
+		&branch.Address,
+		&branch.Active,
+		&branch.Destination,
+		&branch.CreatedAt,
+		&updatedAt,
+	)
+	if err != nil {
+		if err == pgx.ErrNoRows {
+			return nil, fmt.Errorf("branch not found")
+		}
+		return nil, fmt.Errorf("failed to get branch: %w", err)
+	}
+
+	if createdAt.Valid {
+		branch.CreatedAt = createdAt.String
+	}
+	if updatedAt.Valid {
+		branch.UpdatedAt = updatedAt.String
+	}
+
+	return &branch, nil
+}
+
+func (b *branchRepo) GetList(c context.Context, req *user_service.ListBranchRequest) (*user_service.ListBranchResponse, error) {
+
+	var (
+		updatedAt sql.NullString
+		createdAt sql.NullString
+		resp      user_service.ListBranchResponse
+		err       error
+		filter    string
+		params    = make(map[string]interface{})
+	)
+
+	if req.Name != "" {
+		filter += " AND name ILIKE '%' || :name || '%' "
+		params["name"] = req.Name
+	}
+	// Filter with created at range
+	if req.CreatedAtFrom != "" {
+		filter += " AND created_at >= :created_at_from"
+		params["created_at_from"] = req.CreatedAtFrom
+	}
+
+	if req.CreatedAtTo != "" {
+		filter += " AND created_at <= :created_at_to"
+		params["created_at_to"] = req.CreatedAtTo
+	}
+	countQuery := `SELECT count(1) FROM "branches" WHERE "deleted_at" IS NULL AND "active"` + filter
+	q, arr := helper.ReplaceQueryParams(countQuery, params)
+	err = b.db.QueryRow(c, q, arr...).Scan(
+		&resp.Count,
+	)
+
+	if err != nil {
+		return nil, fmt.Errorf("error while scanning count %w", err)
+	}
+
+	query := `
+	SELECT 
+		id,
+		name,
+		phone,
+		photo,
+		delivery_tarif_id,
+		work_hour_start::text,
+		work_hour_end::text,
+		address,
+		active,
+		destination,
+		created_at,
+		updated_at 
+	FROM branches  where "active" and "deleted_at" is null` + filter
+
+	query += " ORDER BY created_at DESC LIMIT :limit OFFSET :offset"
+	params["limit"] = req.Limit
+	params["offset"] = (req.Page - 1) * req.Limit
+
+	q, arr = helper.ReplaceQueryParams(query, params)
+	rows, err := b.db.Query(c, q, arr...)
+	if err != nil {
+		return nil, fmt.Errorf("error while getting rows %w", err)
+	}
+	defer rows.Close()
+
+	for rows.Next() {
+		var branch user_service.Branch
+		err = rows.Scan(
+			&branch.Id,
+			&branch.Name,
+			&branch.Phone,
+			&branch.Photo,
+			&branch.DeliveryTarifId,
+			&branch.WorkHourStart,
+			&branch.WorkHourEnd,
+			&branch.Address,
+			&branch.Active,
+			&branch.Destination,
+			&createdAt,
+			&updatedAt,
+		)
+		if err != nil {
+			return nil, fmt.Errorf("error while scanning branch err: %w", err)
+		}
+
+		if createdAt.Valid {
+			branch.CreatedAt = createdAt.String
+
+		}
+		if updatedAt.Valid {
+			branch.UpdatedAt = updatedAt.String
+		}
+		resp.Branches = append(resp.Branches, &branch)
+	}
+
+	return &resp, nil
+}
+
+func (b *branchRepo) Update(c context.Context, req *user_service.UpdateBranchRequest) (string, error) {
+
+	query := `
+				UPDATE "branches" 
+				SET 
+				"name" = $1,
+				"phone" = $2,
+				"photo" = $3,  
+				"delivery_tarif_id" = $4,
+				"work_hour_start"=$5,
+				"work_hour_end"=$6,
+				"address"=$7,
+				"destination"=$8,
+				"updated_at" = NOW() 
+				WHERE id = $9`
+
+	result, err := b.db.Exec(
+		context.Background(),
+		query,
+		req.Name,
+		req.Phone,
+		req.Photo,
+		req.DeliveryTarifId,
+		req.WorkHourStart,
+		req.WorkHourEnd,
+		req.Address,
+		req.Destination,
+		req.Id,
+	)
+
+	if err != nil {
+		return "", fmt.Errorf("failed to update branch: %w", err)
+	}
+
+	if result.RowsAffected() == 0 {
+		return "", fmt.Errorf("branch with ID %d not found", req.Id)
+	}
+
+	return fmt.Sprintf("branch with ID %d updated", req.Id), nil
+}
+
+func (b *branchRepo) Delete(c context.Context, req *user_service.IdRequest) (resp string, err error) {
+
+	query := `
+				UPDATE "branches" 
+				SET 
+				"active" = false,
+				"deleted_at" = NOW() 
+				WHERE id = $1 AND "deleted_at" IS NULL and "active"`
+
+	result, err := b.db.Exec(
+		context.Background(),
+		query,
+		req.Id,
+	)
+
+	if err != nil {
+		return "", fmt.Errorf("failed to delete branch: %w", err)
+	}
+
+	if result.RowsAffected() == 0 {
+		return "", fmt.Errorf("branch with ID %s not found", req.Id)
+	}
+
+	return "deleted", nil
+}
---
+++ Delivery_Project/user_service/storage/postgres/couriers.go
@@ -0,0 +1,266 @@
+package postgres
+
+import (
+	"context"
+	"database/sql"
+	"fmt"
+
+	user_service "user_service/genproto"
+	"user_service/pkg/helper"
+
+	"github.com/jackc/pgx"
+	"github.com/jackc/pgx/v4/pgxpool"
+)
+
+type courierRepo struct {
+	db *pgxpool.Pool
+}
+
+func NewCourier(db *pgxpool.Pool) *courierRepo {
+	return &courierRepo{
+		db: db,
+	}
+}
+
+func (b *courierRepo) Create(c context.Context, req *user_service.CreateCouriersRequest) (*user_service.Response, error) {
+
+	query := `
+	  INSERT INTO couriers (
+		first_name,
+		last_name,
+		branch_id,  
+		phone,
+		login,
+		password,
+		max_order_count,
+		created_at
+	  ) VALUES (
+		$1, $2, $3, $4, $5, $6,$7, now()
+	  ) RETURNING id`
+
+	var id int
+	err := b.db.QueryRow(c, query,
+		req.Firstname,
+		req.Lastname,
+		req.BranchId,
+		req.Phone,
+		req.Login,
+		req.Password,
+		req.MaxOrderCount,
+	).Scan(&id)
+
+	if err != nil {
+		return nil, fmt.Errorf("failed to create courier: %w", err)
+	}
+
+	return &user_service.Response{Message: fmt.Sprintf("%d", id)}, nil
+}
+func (b *courierRepo) Get(c context.Context, req *user_service.IdRequest) (resp *user_service.Couriers, err error) {
+	var (
+		createdAt sql.NullString
+		updatedAt sql.NullString
+	)
+	query := `
+    SELECT 
+		id,
+		first_name,
+		last_name,
+		branch_id,  
+		phone,
+		login,
+		active,
+		password,
+		max_order_count,
+		created_at,
+		updated_at 
+    FROM couriers 
+    WHERE id = $1 AND "active" AND "deleted_at" IS NULL;`
+
+	courier := user_service.Couriers{}
+
+	err = b.db.QueryRow(c, query, req.Id).Scan(
+		&courier.Id,
+		&courier.Firstname,
+		&courier.Lastname,
+		&courier.BranchId,
+		&courier.Phone,
+		&courier.Login,
+		&courier.Active,
+		&courier.Password,
+		&courier.MaxOrderCount,
+		&createdAt,
+		&updatedAt,
+	)
+	if err != nil {
+		if err == pgx.ErrNoRows {
+			return nil, fmt.Errorf("courier not found")
+		}
+		return nil, fmt.Errorf("failed to get courier: %w", err)
+	}
+	if createdAt.Valid {
+		courier.CreatedAt = createdAt.String
+	}
+
+	if updatedAt.Valid {
+		courier.UpdatedAt = updatedAt.String
+	}
+
+	return &courier, nil
+}
+
+func (b *courierRepo) GetList(c context.Context, req *user_service.ListCouriersRequest) (*user_service.ListCouriersResponse, error) {
+
+	var (
+		updatedAt sql.NullString
+		createdAt sql.NullString
+		resp      user_service.ListCouriersResponse
+		err       error
+		filter    string
+		params    = make(map[string]interface{})
+	)
+
+	if req.Search != "" {
+		filter += " AND (first_name ILIKE '%' || :search || '%' OR last_name ILIKE '%' || :search || '%' OR phone ILIKE '%' || :search || '%') "
+		params["search"] = req.Search
+	}
+	// Filter with created at range
+	if req.CreatedAtFrom != "" {
+		filter += " AND created_at >= :created_at_from"
+		params["created_at_from"] = req.CreatedAtFrom
+	}
+
+	if req.CreatedAtTo != "" {
+		filter += " AND created_at <= :created_at_to"
+		params["created_at_to"] = req.CreatedAtTo
+	}
+
+	countQuery := `SELECT count(1) FROM "users" WHERE "deleted_at" IS NULL AND "active"` + filter
+	q, arr := helper.ReplaceQueryParams(countQuery, params)
+	err = b.db.QueryRow(c, q, arr...).Scan(
+		&resp.Count,
+	)
+
+	if err != nil {
+		return nil, fmt.Errorf("error while scanning count %w", err)
+	}
+
+	query := `
+	SELECT 
+	        id,
+	        first_name,
+	        last_name,
+	        phone,
+			active,
+	        login,
+	        password,
+			max_order_count,
+	    	created_at::text,
+		    updated_at 
+	FROM couriers  where "active" and "deleted_at" is null` + filter
+
+	query += " ORDER BY created_at DESC LIMIT :limit OFFSET :offset"
+	params["limit"] = req.Limit
+	params["offset"] = (req.Page - 1) * req.Limit
+
+	q, arr = helper.ReplaceQueryParams(query, params)
+	rows, err := b.db.Query(c, q, arr...)
+	if err != nil {
+		return nil, fmt.Errorf("error while getting rows %w", err)
+	}
+	defer rows.Close()
+
+	for rows.Next() {
+		var courier = user_service.Couriers{}
+		err = rows.Scan(
+			&courier.Id,
+			&courier.Firstname,
+			&courier.Lastname,
+			&courier.Phone,
+			&courier.Active,
+			&courier.Login,
+			&courier.Password,
+			&courier.MaxOrderCount,
+			&courier.CreatedAt,
+			&updatedAt,
+		)
+		if err != nil {
+			return nil, fmt.Errorf("error while scanning courier err: %w", err)
+		}
+
+		if createdAt.Valid {
+			courier.CreatedAt = createdAt.String
+
+		}
+		if updatedAt.Valid {
+			courier.UpdatedAt = updatedAt.String
+		}
+		resp.Couriers = append(resp.Couriers, &courier)
+	}
+
+	return &resp, nil
+}
+
+func (b *courierRepo) Update(c context.Context, req *user_service.UpdateCouriersRequest) (string, error) {
+
+	query := `
+				UPDATE "couriers" 
+				SET 
+				"first_name" = $1,
+				"last_name" = $2,
+				"branch_id" = $3,  
+				"phone" = $4,
+				"login"=$5,
+				"password"=$6,
+				"max_order_count"=$7,
+				"updated_at" = NOW() 
+				WHERE id = $8`
+
+	result, err := b.db.Exec(
+		context.Background(),
+		query,
+		req.Firstname,
+		req.Lastname,
+		req.BranchId,
+		req.Phone,
+		req.Login,
+		req.Password,
+		req.MaxOrderCount,
+		req.Id,
+	)
+
+	if err != nil {
+		return "", fmt.Errorf("failed to update courier: %w", err)
+	}
+
+	if result.RowsAffected() == 0 {
+		return "", fmt.Errorf("courier with ID %d not found", req.Id)
+	}
+
+	return fmt.Sprintf("courier with ID %d updated", req.Id), nil
+}
+
+func (b *courierRepo) Delete(c context.Context, req *user_service.IdRequest) (resp string, err error) {
+
+	query := `
+				UPDATE "couriers" 
+				SET 
+				"active" = false,
+				"deleted_at" = NOW() 
+				WHERE id = $1 AND "deleted_at" IS NULL and "active"`
+
+	result, err := b.db.Exec(
+		context.Background(),
+		query,
+		req.Id,
+	)
+
+	if err != nil {
+		return "", fmt.Errorf("failed to delete courier: %w", err)
+	}
+
+	if result.RowsAffected() == 0 {
+		return "", fmt.Errorf("courier with ID %s not found", req.Id)
+	}
+
+	return "deleted", nil
+}
