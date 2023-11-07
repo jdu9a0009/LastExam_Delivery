50c3dbaa9193b764dd30f75484bf3b5e61d6fb88
Sarvarbek Fozilov <123530266+jdu9a0009@users.noreply.github.com>
2023-11-07 12:48:52 +0500

    order service added succesfully

---
+++ Delivery_Project/order_service/cmd/main.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	"order_service/config"
+	"order_service/grpc"
+
+	"context"
+	"fmt"
+	"log"
+	"net"
+	"order_service/pkg/logger"
+	"order_service/storage/postgres"
+)
+
+func main() {
+	cfg := config.Load()
+	lg := logger.NewLogger(cfg.Environment, "debug")
+	strg, err := postgres.NewStorage(context.Background(), cfg)
+	if err != nil {
+		log.Fatalf("Failed to connect to database: %v", err)
+	}
+
+	s := grpc.SetUpServer(cfg, lg, strg)
+	lis, err := net.Listen("tcp", fmt.Sprintf(":%d", 50053))
+	if err != nil {
+		log.Fatalf("Failed to listen: %v", err)
+	}
+
+	log.Printf("Server listening at %v", lis.Addr())
+	if err := s.Serve(lis); err != nil {
+		log.Fatalf("Failed to serve: %v", err)
+	}
+}
